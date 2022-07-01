d63e460c1daf456b0636a5b4d47242dbf19f0e1a
emilyb25 <90354208+emilyb25@users.noreply.github.com>
2022-07-01 17:12:36 -0400

    Made the auto route for hap/map. only problem is it is not looking straight b4 shooting at the end

--- TechTigers2022Robot/src/main/java/frc/robot/commands/HapMapAutoRoute.java
+++ TechTigers2022Robot/src/main/java/frc/robot/commands/HapMapAutoRoute.java
@@ -24,20 +24,90 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class HapMapAutoRoute extends SequentialCommandGroup {
-  NewRunMotionProfile mp;
-  NewRunMotionProfile mp1;
+  NewRunMotionProfile forward194TurnLeft90;
+  NewRunMotionProfile forward165;
+  NewRunMotionProfile back100TurnRight90;
+  NewRunMotionProfile fig8x1;
+  NewRunMotionProfile fig8x2;
+  NewRunMotionProfile fig8x3;
+  NewRunMotionProfile fig8x4;
+  NewRunMotionProfile fig8x5;
+  NewRunMotionProfile fig8x6;
+  NewRunMotionProfile fig8x7;
+  NewRunMotionProfile fig8x8;
+  NewRunMotionProfile forward100TurnRight90;
   /** Creates a new AutonomousDCommand. */
   public HapMapAutoRoute(RobotOdometry odometry, Drive driveTrain) {
-    mp = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d(0)), 0,
+    forward194TurnLeft90 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d(0)), 0,
         List.of(),
-        new Pose2d(Units.inchesToMeters(10), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, false, false);
+        new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(0), Rotation2d.fromDegrees(90)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-   mp1 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(10), Units.inchesToMeters(0), new Rotation2d(0)), 0,
+   forward165 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(0), new Rotation2d(90)), 0,
         List.of(),
-        new Pose2d(Units.inchesToMeters(-82.5), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
+        new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(165), Rotation2d.fromDegrees(90)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    back100TurnRight90 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(165), new Rotation2d(90)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(65), Rotation2d.fromDegrees(0)), 0, true, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    fig8x1 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(65), new Rotation2d(0)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(269), Units.inchesToMeters(-10), Rotation2d.fromDegrees(0)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //fig8x2 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(269), Units.inchesToMeters(-10), new Rotation2d(0)), 0,
+        //List.of(),
+        //new Pose2d(Units.inchesToMeters(344), Units.inchesToMeters(65), Rotation2d.fromDegrees(90)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    fig8x3 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(269), Units.inchesToMeters(-10), new Rotation2d(0)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(419), Units.inchesToMeters(140), Rotation2d.fromDegrees(0)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    fig8x4 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(419), Units.inchesToMeters(140), new Rotation2d(0)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(494), Units.inchesToMeters(65), Rotation2d.fromDegrees(-90)), 0, false, false); 
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        
+    fig8x5 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(494), Units.inchesToMeters(65), new Rotation2d(-90)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(419), Units.inchesToMeters(-10), Rotation2d.fromDegrees(-180)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //fig8x6 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(419), Units.inchesToMeters(-10), new Rotation2d(-180)), 0,
+        //List.of(),
+        //new Pose2d(Units.inchesToMeters(344), Units.inchesToMeters(65), Rotation2d.fromDegrees(-270)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+   fig8x7 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(419), Units.inchesToMeters(-10), new Rotation2d(-180)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(269), Units.inchesToMeters(140), Rotation2d.fromDegrees(-180)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  
+   fig8x8 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(269), Units.inchesToMeters(140), new Rotation2d(-180)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(65), Rotation2d.fromDegrees(-180)), 0, false, false);
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    forward100TurnRight90 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(65), new Rotation2d(-180)), 0,
+        List.of(),
+        new Pose2d(Units.inchesToMeters(194), Units.inchesToMeters(165), Rotation2d.fromDegrees(90)), 0, false, false);
+
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))), new WaitCommand(2), mp, new BallShooterCommand(1,0),mp1);
+      new ParallelDeadlineGroup(
+        new SequentialCommandGroup(
+          new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))), forward194TurnLeft90, forward165, new WaitCommand(10), back100TurnRight90,
+          fig8x1, fig8x3, fig8x4, fig8x5, fig8x7, fig8x8, forward100TurnRight90, new WaitCommand(2), new BallShooterCommand(1,1))),
+      
+        new IntakeStopCommand()
+    );
 }
 }
+
+//75x75
