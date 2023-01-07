7ef190ddc6297c31d4a6582ed8ff98417df6b282
emilyb25 <90354208+emilyb25@users.noreply.github.com>
2023-01-07 14:55:51 -0500

    imported 2023 robot into 2023 wpilib

---
+++ TechTigers2023Robot-Imported/src/main/java/frc/robot/OI.java
@@ -0,0 +1,90 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.Turn90DegreesCommand;
+//import frc.robot.commands.AutonomousDriveCommand;
+//import frc.robot.commands.DriveTargetCommand;
+//import frc.robot.commands.BallFlushCommand;
+
+/**
+ * This class is the glue that binds the controls on the physical operator
+ * interface to the commands and command groups that allow control of the robot.
+ */
+public class OI {
+  //// CREATING BUTTONS
+  // One type of button is a joystick button which is any button on a
+  //// joystick.
+  // You create one by telling it which joystick it's on and which button
+  // number it is.
+  public Joystick driverStick = new Joystick(RobotMap.driverJoystickPort);
+  public Joystick operatorStick = new Joystick(RobotMap.operatorJoystickPort);
+  public JoystickButton turboButton;
+  public JoystickButton driveStraightButton;
+  public JoystickButton turnLeft90Button;
+  public JoystickButton turnRight90Button; 
+  public JoystickButton turnLeft180Button;
+  public JoystickButton turnRight180Button;
+  public JoystickButton intakeStopButton;
+  public JoystickButton slidingClimbButton;
+  public JoystickButton verticalClimbDownButton;
+  public JoystickButton verticalClimbUpButton;
+  public JoystickButton beltcroShooterButton;
+  public JoystickButton manualSlidingClimbButton;
+  public JoystickButton slidingClimbReverseButton;
+  public JoystickButton slidingTraversalDownButton;
+  public JoystickButton climbLockButton;
+  public JoystickButton beltcroReverseButton;
+
+  public JoystickButton slidingShortClimbButton;
+  public JoystickButton slidingShortClimbReverseButton;
+ 
+
+  public JoystickButton limeLightButton;
+  public JoystickButton climbLockLeftButton;
+  public JoystickButton climbLockRightButton;
+  public JoystickButton climbUnlockLeftButton;
+  public JoystickButton climbUnlockRightButton;
+  public JoystickButton intakeOverrideButton;
+  public JoystickButton ballShooterButton;
+  public JoystickButton highGoalShooterButton;
+  //public JoystickButton turretButton;
+
+  public OI(){
+
+  turboButton = new JoystickButton(driverStick, RobotMap.turboButtonNumber);
+  driveStraightButton = new JoystickButton(driverStick, RobotMap.driveStraightButtonNumber);
+  turnLeft90Button = new JoystickButton(driverStick, RobotMap.turnLeft90ButtonNumber);
+  turnRight90Button = new JoystickButton(driverStick, RobotMap.turnRight90ButtonNumber);
+  turnLeft180Button = new JoystickButton(driverStick, RobotMap.turnLeft180ButtonNumber);
+  turnRight180Button = new JoystickButton(driverStick, RobotMap.turnRight180ButtonNumber);
+  intakeStopButton = new JoystickButton(operatorStick, RobotMap.intakeStopButtonNumber);
+  manualSlidingClimbButton = new JoystickButton(driverStick, RobotMap.manualSlidingClimbButtonNumber);
+  verticalClimbUpButton = new JoystickButton(operatorStick, RobotMap.verticalClimbUpButtonNumber);
+  verticalClimbDownButton = new JoystickButton(operatorStick, RobotMap.verticalClimbDownButtonNumber); //change to operatorStick when we have both joystick
+  //limeLightButton = new JoystickButton(driverStick, RobotMap.limeLightButtonNumber);
+
+
+  slidingShortClimbButton = new JoystickButton(operatorStick, RobotMap.slidingShortClimbButtonNumber);
+  slidingShortClimbReverseButton = new JoystickButton(operatorStick, RobotMap.slidingShortClimbReverseButtonNumber);
+
+  //climbLockRightButton = new JoystickButton(operatorStick, RobotMap.climbLockRightButtonNumber);
+  //climbLockLeftButton = new JoystickButton(operatorStick, RobotMap.climbLockRightButtonNumber);
+  //climbUnlockLeftButton = new JoystickButton(operatorStick, RobotMap.climbUnlockLeftButtonNumber);
+  //climbUnlockRightButton = new JoystickButton(operatorStick, RobotMap.climbUnlockRightButtonNumber);
+  //turretButton = new JoystickButton(operatorStick, RobotMap.turretButtonNumber);
+
+  turnLeft90Button.whenPressed(new Turn90DegreesCommand());
+  turnRight90Button.whenPressed(new Turn90DegreesCommand());
+  turnLeft180Button.whenPressed(new Turn90DegreesCommand());
+  turnRight180Button.whenPressed(new Turn90DegreesCommand());
+
+  }
+  
+}
---
+++ TechTigers2023Robot-Imported/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,88 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController;
+import frc.robot.subsystems.RobotOdometry;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.Drive;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.*;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.subsystems.*;
+import edu.wpi.first.cameraserver.CameraServer;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and button mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems
+  public static Drive drive;
+  public static OI oi;
+
+  private RobotOdometry odometry;
+
+  private final SendableChooser<Command> autoChooser = new SendableChooser<Command>();
+
+  // The driver's controller
+  XboxController m_driverController = new XboxController(OIConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+
+    // VERY IMPORTANT:   drive need be created before oi since oi creates Turn90DegreesCommand object in which need drive object
+    drive = new Drive();
+    oi = new OI();  // need be after drive object
+    //always keep OI last
+
+    // Configure the button bindings
+    configureButtonBindings();
+
+    drive.resetEncoders();
+    drive.resetHeading();
+    odometry = new RobotOdometry(drive, drive.getPigeonIMU());
+    odometry.resetOdometry();
+    drive.setDefaultCommand(new ManualDriveCommand());
+
+    CameraServer.startAutomaticCapture(0);
+
+    //autoChooser.addOption("Auto (Shot and Move)", new NewCommandGroup(odometry, drive));
+        
+    //autoChooser.addOption("Galactic Search (A)", new RunGalacticSearchA(odometry, drive));
+    //autoChooser.addOption("Galactic Search (B)", new RunGalacticSearchB(odometry, drive));
+    
+
+    autoChooser.addOption("1 High Goal", new AutonomousACommand(odometry, drive));
+    //autoChooser.addOption("2 Ball C", new AutonomousCCommand(odometry, drive));
+    //autoChooser.addOption("2 Ball B", new AutonomousBCommand(odometry, drive));
+    //autoChooser.setDefaultOption("MoveAndShootLow", new AutonomousDCommand(odometry, drive));
+    //autoChooser.addOption("MoveAndShootLow", new AutonomousDCommand(odometry, drive));
+    //autoChooser.setDefaultOption("Complicated Auto Route", new HapMapAutoRoute(odometry, drive));
+    
+    SmartDashboard.putData("Auto Mode", autoChooser);
+
+  }
+
+  /**
+   * Use this method to define your button->command mappings. Buttons can be created by
+   * instantiating a {@link GenericHID} or one of its subclasses ({@link
+   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then calling passing it to a
+   * {@link JoystickButton}.
+   */
+  private void configureButtonBindings() {
+   
+  }
+
+  public Command getAutonomousCommand() {
+    return autoChooser.getSelected();
+  }
+
+}
