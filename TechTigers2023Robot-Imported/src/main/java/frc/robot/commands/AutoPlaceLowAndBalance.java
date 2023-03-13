9141171b911cdcb93aa98d2601f659e12041ca74
emilyb25 <90354208+emilyb25@users.noreply.github.com>
2023-03-13 19:14:04 -0400

    tuned up some values

--- TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceLowAndBalance.java
+++ TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceLowAndBalance.java
@@ -25,6 +25,7 @@ import frc.robot.commands.PneumaticsGrabbingCommand;
 import frc.robot.commands.ArmSetPositionsCommand;
 import frc.robot.commands.AutoPneumatics;
 import frc.robot.commands.AutoWrist;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 
 
 public class AutoPlaceLowAndBalance extends SequentialCommandGroup {
@@ -36,7 +37,7 @@ public class AutoPlaceLowAndBalance extends SequentialCommandGroup {
   public AutoPlaceLowAndBalance(RobotOdometry odometry, Drive driveTrain) {
     mp = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d()), 0,
         List.of(),
-        new Pose2d(Units.inchesToMeters(-150), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
+        new Pose2d(Units.inchesToMeters(-160), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
 
     //mp1 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(-150), Units.inchesToMeters(0), new Rotation2d()), 0,
        // List.of(),
@@ -53,10 +54,36 @@ public class AutoPlaceLowAndBalance extends SequentialCommandGroup {
     //addCommands(new InstantCommand(() -> odometry.setPosition(new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d()))), mp, new WaitCommand(2), mp1);
 
    //SlidingClimbHooksCommand.climbNumber = 1;
-       addCommands(
-              new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))),
-              new ArmSetPositionsCommand(2000), new AutoWrist(1), new AutoPneumatics(1), new AutoWrist(2), new AutoPneumatics(2),
-              mp, new WaitCommand(0.5), new AutoBalanceCommand());
+       //addCommands(
+             //new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))),
+              //new ArmSetPositionsCommand(2000), new AutoWrist(1), new AutoPneumatics(1), new AutoWrist(2), new AutoPneumatics(2),
+              //mp, new WaitCommand(0.5), new AutoBalanceCommand());
+
+              addCommands(
+                new ParallelCommandGroup(
+                  new ArmSetPositionsCommand(2000), // raise arm a little bit, 1.5 seconds
+                  new  AutoWrist(1) // lowers wrist , 2 seconds
+                )
+           );
+      
+           addCommands( new AutoPneumatics(1 ) ); // opens pnematic to drop, 1 second
+      
+           addCommands(
+                new ParallelCommandGroup(
+                  new AutoWrist(2), // raise wrist, 2 seconds, don't wait for full 2 seoonds to do next command
+                  new  SequentialCommandGroup (
+                    new WaitCommand(1),   // wait for 1 second for wrist to raise above group
+                    new ParallelCommandGroup(
+                      new AutoPneumatics(2),  // 1 second
+                      new  SequentialCommandGroup(
+                          mp,                           // estimate about 4 seconds: 1.3 meter/second x 4 = 5.2 meter (~157 inches), after ~ 4 seconds in autonomous
+                          new WaitCommand(1),   // wait for 1 second for the balance swing back to nornal
+                          new AutoBalanceCommand()       // about 5 to 6 seconds left to auto balance
+                      )
+                    )
+                  )
+                )
+           );
   }
 
   
