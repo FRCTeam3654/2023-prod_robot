15df1280032038a9bd09564bc3f97d4e24aea56b
zjwu46 <zjwu46@gmail.com>
2023-03-16 23:48:31 -0400

    prepare place mid commands

--- TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceMidCubeAndMoveRight.java
+++ TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceMidCubeAndMoveRight.java
@@ -25,25 +25,27 @@ import frc.robot.commands.PneumaticsGrabbingCommand;
 import frc.robot.commands.ArmSetPositionsCommand;
 import frc.robot.commands.ManualArmCommand;
 import frc.robot.commands.ArmJoustCommand;
-import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoPlaceMidAndMove extends SequentialCommandGroup {
-  /** Creates a new AutoPlaceMidAndBalance. */
+public class AutoPlaceMidCubeAndMoveRight extends SequentialCommandGroup {
+  /** Creates a new AutoPlaceMidCubeAndMoveRight. */
+  
   NewRunMotionProfile mp;
 
-  public AutoPlaceMidAndMove(RobotOdometry odometry, Drive driveTrain) {
+  public AutoPlaceMidCubeAndMoveRight(RobotOdometry odometry, Drive driveTrain) {
+    
+    
+    // assume the right is the negative Y (robot front facing driver station)
+
     mp = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d()), 0,
-        List.of(),
-        new Pose2d(Units.inchesToMeters(-160), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-   // addCommands(
-   //   new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))), new ArmSetPositionsCommand(), new ArmJoustCommand(1), new WristMotionMagic(), new PneumaticsGrabbingCommand(1), new WristMotionMagic(), new ArmJoustCommand(2), new PneumaticsGrabbingCommand(2), new ArmSetPositionsCommand(), mp
-   // );
+        List.of(new Translation2d(Units.inchesToMeters(-20), Units.inchesToMeters(-9)), new Translation2d(Units.inchesToMeters(-40), Units.inchesToMeters(-18))),
+        new Pose2d(Units.inchesToMeters(-160), Units.inchesToMeters(-18), Rotation2d.fromDegrees(0)), 0, true, false);
+   
 
    
         addCommands(
@@ -78,23 +80,5 @@ public class AutoPlaceMidAndMove extends SequentialCommandGroup {
 
         );
 
-        /* 
-        addCommands( new AutoPneumatics(1 ) ); // opens pnematic to drop, 1 second
-
-        addCommands(
-            new ParallelCommandGroup(
-              new AutoWrist(2), // raise wrist, 2 seconds, don't wait for full 2 seoonds to do next command
-              new ArmJoustCommand(2),  // NEW: 2 seconds for telescoping arm to retract
-              new  SequentialCommandGroup (
-                new WaitCommand(1),   // wait for 1 second for wrist to raise above group
-                new ArmSetPositionsCommand(), // lower arm to near bottom, 2.5 seconds
-                new ParallelCommandGroup(
-                  new AutoPneumatics(2),  // 1 second   
-                  mp                           // estimate about 4 seconds: 1.3 meter/second x 4 = 5.2 meter (~157 inches), after ~ 4 seconds in autonomous
-                )
-              )
-            )
-        );
-        */
   }
 }
