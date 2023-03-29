340e85ed4a311d153a6952d50dbaa61d81575a7a
zjwu46 <zjwu46@gmail.com>
2023-03-28 23:00:13 -0400

    Auto mid cube routine

--- TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceMidAndMove.java
+++ TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceMidAndMove.java
@@ -27,6 +27,8 @@ import frc.robot.commands.ManualArmCommand;
 import frc.robot.commands.ArmJoustCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj.DriverStation;
+import frc.robot.RobotMap;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -39,13 +41,50 @@ public class AutoPlaceMidAndMove extends SequentialCommandGroup {
     mp = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d()), 0,
         List.of(),
         new Pose2d(Units.inchesToMeters(-160), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-   // addCommands(
-   //   new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))), new ArmSetPositionsCommand(), new ArmJoustCommand(1), new WristMotionMagic(), new PneumaticsGrabbingCommand(1), new WristMotionMagic(), new ArmJoustCommand(2), new PneumaticsGrabbingCommand(2), new ArmSetPositionsCommand(), mp
-   // );
-
    
+
+        addCommands(
+          new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))) ,
+          new ParallelCommandGroup(
+              new ArmSetPositionsCommand(3, 2200, 1.0),
+              new  SequentialCommandGroup (
+                  new WaitCommand(0.3),
+                  new AutoArmJoustCommand(1)  // NEW: 2 seconds for telescoping arm to extend
+              )
+          ),
+          new ParallelCommandGroup(
+            new ArmSetPositionsCommand(1, 2), // raise arm to full distance, 2 seconds
+            //new  SequentialCommandGroup (
+              //new ParallelCommandGroup(
+                new  SequentialCommandGroup (
+                  new WaitCommand(0.5), // vs 0.8
+                  new  AutoWrist(1, 1.1 * RobotMap.wristFullUpDistance)// lowers wrist , 1.5 seconds
+                )
+              //)
+            //)
+          ),
+          new AutoPneumatics(1, 0.5),
+          new ParallelCommandGroup(
+            new AutoWrist(2), // raise wrist, 1.5 seconds, don't wait for full 2 seoonds to do next command
+            new AutoArmJoustCommand(2),    // NEW: 2 seconds for telescoping arm to retract
+            new AutoPneumatics(2),  // 1 second  
+            new  SequentialCommandGroup (
+              new WaitCommand(0.5),
+              new ArmSetPositionsCommand(2, 2.0) // lower arm to near bottom, 2 seconds 
+            ),
+            new  SequentialCommandGroup (
+                new WaitCommand(0.1),
+                mp      // estimate about 4 seconds: 1.3 meter/second x 4 = 5.2 meter (~157 inches), after ~ 4 seconds in autonomous
+            )
+          )
+
+      );
+
+
+
+
+
+       /* 
         addCommands(
             new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))) ,
         
@@ -77,24 +116,7 @@ public class AutoPlaceMidAndMove extends SequentialCommandGroup {
             )
 
         );
-
-        /* 
-        addCommands( new AutoPneumatics(1 ) ); // opens pnematic to drop, 1 second
-
-        addCommands(
-            new ParallelCommandGroup(
-              new AutoWrist(2), // raise wrist, 2 seconds, don't wait for full 2 seoonds to do next command
-              new ArmJoustCommand(2),  // NEW: 2 seconds for telescoping arm to retract
-              new  SequentialCommandGroup (
-                new WaitCommand(1),   // wait for 1 second for wrist to raise above group
-                new ArmSetPositionsCommand(), // lower arm to near bottom, 2.5 seconds
-                new ParallelCommandGroup(
-                  new AutoPneumatics(2),  // 1 second   
-                  mp                           // estimate about 4 seconds: 1.3 meter/second x 4 = 5.2 meter (~157 inches), after ~ 4 seconds in autonomous
-                )
-              )
-            )
-        );
-        */
+       */
+       
   }
 }
