dcc53443c472f71ad35b2cec7f93fba2cfe86049
zjwu46 <zjwu46@gmail.com>
2023-04-05 09:45:50 -0400

    Update AutoPlaceHighAndBalance.java

--- TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceHighAndBalance.java
+++ TechTigers2023Robot-Imported/src/main/java/frc/robot/commands/AutoPlaceHighAndBalance.java
@@ -47,49 +47,49 @@ public class AutoPlaceHighAndBalance extends SequentialCommandGroup {
 
     mp = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(0), Units.inchesToMeters(0), new Rotation2d()), 0,
       List.of(),
-      new Pose2d(Units.inchesToMeters(-160), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
+      new Pose2d(Units.inchesToMeters(-140), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, true, false);
 
-    mp1 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(-150), Units.inchesToMeters(0), new Rotation2d()), 0,
+    mp1 = new NewRunMotionProfile(driveTrain, odometry, new Pose2d(Units.inchesToMeters(-155), Units.inchesToMeters(0), new Rotation2d()), 0,
       List.of(),
-      new Pose2d(Units.inchesToMeters(-65), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, false, false);
+      new Pose2d(Units.inchesToMeters(-60), Units.inchesToMeters(0), Rotation2d.fromDegrees(0)), 0, false, false);
 
       addCommands(
           new InstantCommand(() -> odometry.setPosition(new Pose2d( Units.inchesToMeters(0),  Units.inchesToMeters(0), new Rotation2d()))) ,
-          /*new ParallelCommandGroup(
-              new ArmSetPositionsCommand(3, 2200, 1.0)
-              new  SequentialCommandGroup (
-                  new WaitCommand(0.3),
-                  new AutoArmJoustCommand(1)  // NEW: 2 seconds for telescoping arm to extend
-              )
-              
-          ),
-          */
+          
           new ParallelCommandGroup(
-            new ArmSetPositionsCommand(1, 2), // raise arm to full distance, 2 seconds
-            new  SequentialCommandGroup (
-                  new WaitCommand(0.5), // vs 0.8
-                  new  AutoWrist(1, 1.1 * RobotMap.wristFullUpDistance)// lowers wrist , 1.5 seconds
-            )
+            new AutoArmSetPositionsCommand(1, 1.2 * RobotMap.armFullUpDistance, 2), // raise arm to full distance, 2 seconds
+                new  SequentialCommandGroup (
+                  new WaitCommand(0.1), // vs 0.5
+                  new  AutoWrist(1, 1.5 * RobotMap.wristFullUpDistance)// lowers wrist , 1.5 seconds
+                )
           ),
-          //new AutoPneumatics(1, 0.5),
+
+          new WaitCommand(1), // give the arm a little time to stablize
+            
           // --- shoot the cube via roller here ---
-          new AutoIntakeWheelsCommand(3),
+
           new ParallelCommandGroup(
-            new AutoWrist(2), // raise wrist, 1.5 seconds, don't wait for full 2 seoonds to do next command
-            //new AutoArmJoustCommand(2),    // NEW: 2 seconds for telescoping arm to retract
-            //new AutoPneumatics(2),  // 1 second
-            new AutoIntakeWheelsCommand(0),  
-            new  SequentialCommandGroup (
-              new WaitCommand(0.5),
-              new ArmSetPositionsCommand(2, 2.0) // lower arm to near bottom, 2 seconds 
-            ),
-            new  SequentialCommandGroup(
-                mp,                           // estimate about 4 seconds: 1.3 meter/second x 4 = 5.2 meter (~157 inches), after ~ 4 seconds in autonomous
-                new WaitCommand(0.8),   // wait for 0.8 second for the balance swing back to nornal
-                mp1,                          // drive towards the platform via mp instead of percent output in autobalance
-                new AutoBalance2Command()       // about 5 to 6 seconds left to auto balance
+                new AutoIntakeWheelsCommand(3),
+                new  SequentialCommandGroup(      
+                  new WaitCommand(0.5), 
+                  new ParallelCommandGroup(
+                        new AutoWrist(2), // raise wrist, 1.5 seconds, don't wait for full 2 seoonds to do next command
+                        new AutoIntakeWheelsCommand(0),  
+                        new  SequentialCommandGroup (
+                          new WaitCommand(0.5),
+                          new AutoArmSetPositionsCommand(2, 2200, 2.0) // lower arm to near bottom, 2 seconds 
+                        ),
+                        new  SequentialCommandGroup(      
+                              mp,                           // estimate about 4 seconds: 1.3 meter/second x 4 = 5.2 meter (~157 inches), after ~ 4 seconds in autonomous
+                              new WaitCommand(0.5),   // wait for 1 second for the balance swing back to nornal
+                              mp1,                          // drive towards the platform via mp instead of percent output in autobalance
+                              new AutoBalance2Command()       // about 5 to 6 seconds left to auto balance
+                        )
+             
+                  )
+                )
             )
-          )
+
 
       );
 
